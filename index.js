357dfd1979df142bce214b50448dcf00ec76b4f2
punctual-enhancer-1584 <amal.jasen@gmail.com>
2019-09-18 02:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 "use strict";
 
 function takeANumber(deliLine,newPerson) {
+
+  deliLine.push(newPerson);
   
+  return `Welcome, ${newPerson}. You are number ${deliLine.length} in line.`;
+}
+
+function nowServing (deliLine) {
+  const emptyLineMsg="There is nobody waiting to be served!";
+  return (deliLine.length)? deliLine.shift() : emptyLineMsg;
 }
